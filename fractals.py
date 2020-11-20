67679c732d7902460192b1aab66718d4ce8217d6
Stephen Burke <se7enack@users.noreply.github.com>
2020-11-20 17:14:03 -0500

    Update fractals.py

--- fractals.py
+++ fractals.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import matplotlib.pyplot as plt
 import numpy as np
 import random
@@ -17,7 +18,7 @@ def plotter(n, thresh, max_steps=25):
     mx = 2.48 / (n-1)
     my = 2.26 / (n-1)
     mapper = lambda x,y: (mx*x - 2, my*y - 1.13)
-    img=np.full((n,n), 255)
+    img=np.full((n,n), 256)
     for x in range(n):
         for y in range(n):
             it = get_iter(complex(*mapper(x,y)), thresh=thresh, max_steps=max_steps)
@@ -31,8 +32,8 @@ cmap=random.choice(cmaps)
 
 print("Random cmap color chosen from list is: ", cmap)
 n = randrange(2000)
-img = plotter(n, thresh=randrange(100), max_steps=randrange(100))
+img = plotter(n, thresh=4, max_steps=randrange(150))
 plt.imshow(img, cmap=cmap)
 plt.axis("off")
 plt.show()
-   
+    
